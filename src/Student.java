67e7f64d9f48a74c8a011c4bdf1f900d1107b5c6
Zack Laine <rumtehk_uh@yahoo.com>
2024-03-31 09:12:51 -0400

    Fixed regex for delimiting input

--- src/Student.java
+++ src/Student.java
@@ -11,6 +11,41 @@ public class Student {
         this.grade = grade;
     }
 
+    public Student() {
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Integer studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public Double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
--- src/StudentDataService.java
+++ src/StudentDataService.java
@@ -1,14 +1,40 @@
 import java.util.*;
+import java.io.*;
 
 public class StudentDataService {
-    private ArrayList<Student> allStudents = new ArrayList<Student>();
+    private List<Student> allStudents = new ArrayList<Student>();
     private String outputFiles;
-    private Integer NUM_COURSES = 3;
+    private Integer numCourses = 3; //hardcoded for now, add in assigning based on unique course IDs?
+    private String INPUT_FILE = "student-master-list.csv";
 
-//    ArrayList<Student> readFile(){
-//
-//
-//    }
+    List<Student> readFile(){
+        try (Scanner scanner = new Scanner(new File(INPUT_FILE))){
+            scanner.useDelimiter(",|\\r\\n");
+            scanner.nextLine();
+            Student newStudent;
+
+            while(scanner.hasNext()){
+                newStudent = new Student();
+                System.out.println("Student: ");
+                newStudent.setStudentID(Integer.parseInt(scanner.next()));
+                newStudent.setStudentName(scanner.next());
+                newStudent.setCourse(scanner.next());
+                newStudent.setGrade(Double.parseDouble(scanner.next()));
+                System.out.println(newStudent.toString());
+                System.out.println("End student.");
+
+
+
+                allStudents.add(newStudent);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found: " + e.getMessage());
+        }
+
+        return allStudents;
+
+    }
 
     //sorts all the students in the master list into their respective Course list
     void courseSort(){
